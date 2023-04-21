ceec39631e4dc6f276077a90537f46c310b54191
Chris MacNamara <cpmacnamara@gmail.com>
2023-04-21 11:00:35 -0400

    add embedded commentSchema

--- models/post.js
+++ models/post.js
@@ -2,6 +2,13 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema({
+  content: String,
+  owner: {type: Schema.Types.ObjectId, ref: "Profile"}
+}, {
+  timestamps: true
+})
+
 const postSchema = new Schema({
   mainContent: String,
   summary: String,
