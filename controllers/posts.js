82f038c213eda2d950745bdff80fbb2c9b9f629b
Chris MacNamara <cpmacnamara@gmail.com>
2023-04-21 14:55:08 -0400

    add new post route and view

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,6 +2,7 @@ import { Post } from '../models/post.js'
 
 function index(req, res) {
   Post.find({})
+  .populate('owner')
   .then(posts => {
     res.render('posts/index', {
       posts,
@@ -14,6 +15,30 @@ function index(req, res) {
   })
 }
 
+function newPost(req,res) {
+  res.render('posts/new', {
+    title: 'New Post'
+  })
+}
+
+function create(req,res) {
+  req.body.resolved = !!req.body.resolved;
+  console.log('Req.body', req.body);
+  for(let key in req.body) {
+    if(req.body[key] === '') delete req.body[key]
+  }
+  Post.create(req.body)
+  .then(post => {
+    res.redirect('/posts')
+  })
+  .catch(err => {
+    console.log(err);
+    res.redirect('/');
+  })
+}
+
 export {
-  index
+  index,
+  newPost as new,
+  create
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,9 @@ import * as postsCtrl from '../controllers/posts.js'
 const router = Router()
 
 router.get('/', postsCtrl.index)
+router.get('/new', postsCtrl.new)
+
+router.post('/', postsCtrl.create)
 
 export {
   router
