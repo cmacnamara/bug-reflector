162cc4c20be5b1e3b6033b619b54c0092101fe72
Chris MacNamara <cpmacnamara@gmail.com>
2023-04-26 14:52:14 -0400

    add admin property to user model

--- models/post.js
+++ models/post.js
@@ -13,7 +13,6 @@ const commentSchema = new Schema({
 const postSchema = new Schema({
   mainContent: String,
   summary: String,
-  ////technologies: [String],
   resolved: Boolean,
   reflection: String,
   summary: String,
--- models/user.js
+++ models/user.js
@@ -5,6 +5,10 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
   email: String,
   googleId: String,
+  admin: {
+    type: Boolean,
+    default: false,
+  },
   profile: { type: Schema.Types.ObjectId, ref: 'Profile' },
 }, {
   timestamps: true,
